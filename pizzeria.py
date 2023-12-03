7ae876a8279452ffe6e5fd66cb344ecc063aeb12
DjMix22 <111224217+DjMix22@users.noreply.github.com>
2023-12-03 23:10:57 +0700

    Update pizzeria.py
    3-th commit. Changes:
    - add decorator for check
    - pizza and sauce menus are combined

--- pizzeria.py
+++ pizzeria.py
@@ -1,53 +1,70 @@
-def choose_pizza(number, prices_user):
+def my_decorator(pizzeria):
+    def wrapper():
+        arr = pizzeria()
+        print("-----Чек-----")
+        print(arr[0])
+        print(arr[1])
+        print(arr[2])
+        print("-----Спасибо за покупку-----")
+    return wrapper
+
+
+def choose_product(number, prices_user):
     if number == "1":
         print("Пепперони! Отличный выбор")
         prices_user[599.99] = "Пепперони"
 
     elif number == "2":
         print("Сырная! Отличный выбор")
-        prices_user[549.99] = "Сырная"
+        prices_user[549.99] = "Сырная пицца"
 
     elif number == "3":
         print("Timosha pizza! Прекрасный выбор!")
         prices_user[699.99] = "Timosha pizza"
 
-    else:
-        print("Этой пиццы нет в нашем ассортименте :(")
-
-
-def choose_sauce(number, prices_user):
-    if number == "1":
+    elif number == "4":
         print("Хорошо!")
-    elif number == "2":
+
+    elif number == "5":
         print("Соус добавлен!")
-        prices_user[59.99] = "Сырный"
-    elif number == "3":
+        prices_user[59.99] = "Сырный соус"
+
+    elif number == "6":
         print("Соус добавлен!")
         prices_user[69.99] = "Кетчуп"
-    elif number == "4":
-        print("О наличии этого соуса ходят легенды, говорят, он был придуман Камоз...")
+
+    elif number == "7":
+        print("О наличии этого соуса ходят легенды...")
+        prices_user[99999.99] = "Mystery sauce"
+
     else:
-        print("Такого соуса нет в нашем ассортименте :(")
+        print("Этого блюда нет в нашем ассортименте :(")
 
 
 def pay(value, prices_user):
     if value == "1":
-        return True
+        value_new = True
     elif value == "2":
-        return False
-        print("Сумма ваших товаров равна: " + str(sum(prices_user)))
+        value_new = False
+
+        print(f"Сумма ваших товаров равна: {sum(prices_user)}")
+
+        global pay
         pay = input("Выберите оплату! 1) Наличные 2) Карта: ")
+
+        global money
         if pay == "1":
             money = int(input("Положите деньги и мы вернем сдачу!: "))
             if money == sum(prices_user):
-                print("Оплата закончена! Удачного дня!")
+                print("Оплата закончена!  Удачного дня!")
             elif money > sum(prices_user):
-                print("Ваша сдача: " + str(money - sum(prices_user)))
+                print(f"Ваша сдача: {money - sum(prices_user)}")
                 print("Оплата закончена! Удачного дня!")
             else:
                 print("Вам не хватает!")
                 exit()
         elif pay == "2":
+            money = sum(prices_user)
             print("Приложите карту!\nОплата окончена! Удачного дня")
         else:
             print("Такого варианта оплаты нет в нашей сети пиццерий :(")
@@ -55,9 +72,12 @@ def pay(value, prices_user):
     else:
         print("До свидания!")
         exit()
+    return value_new
 
 
-def pizzeria(value  = True):
+@my_decorator
+def pizzeria():
+    value = True
     prices_user = {}
     while value:
         text = ""
@@ -66,20 +86,14 @@ def pizzeria(value  = True):
         else:
             for i in prices_user:
                 text += str(prices_user[i]) + " "
-        print("Вас приветствует пиццерия Тимоша джонс!\nВыберите свою любимую итальянскую пиццу!\nВ вашей корзине: " + str(text))
+        print(f"Вас приветствует пиццерия Тимоша джонс!\nВыберите свои любимые блюда!\nВ вашей корзине: {text}")
+        print("Пиццы:\n1) Пепперони! 599,99₽\n2) Сырная пицца! 549,99₽\n3) Timosha pizza! 699,99₽\nСоусы:\n4) Без "
+              "соуса\n5) Сырный! 59,99₽\n6) Кетчуп! 69,99₽\n7) Mystery sauce! 99999,99₽")
 
-        print("1) Пепперони! 599,99₽\n2) Сырная пицца! 549,99₽\n3) Timosha pizza! 699,99₽")
+        choose_product(input("Введите номер блюда!: "), prices_user)
 
-        choose_pizza(input("Напишите номер своей любимой пиццы!: "), prices_user)
+        value = pay(input("Продолжить выбор вкуснейших пицц? 1) Да 2) Нет: "), prices_user)
 
-        print("Выберите соус:\n1) Без соуса\n2) Сырный 59,99₽\n3) Кетчуп 69,99₽\n4) Mystery sauce 99999,99₽")
-
-        choose_sauce(input("Введите номер соуса!: "), prices_user)
-
-        value= pay(input("Продолжить выбор вкуснейших пицц? 1) Да 2) Нет: "), prices_user)
-
+        return [pay, money, sum(prices_user)]
 
 pizzeria()
-
-
-pizzeria(True)

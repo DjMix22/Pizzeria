d6f6c68e9ffbd4913beaed8b79e884504d9a2364
DjMix22 <111224217+DjMix22@users.noreply.github.com>
2023-12-09 10:03:41 +0700

    Update pizzeria.py
    Changes:
    - update decorator
    - add json file

--- pizzeria.py
+++ pizzeria.py
@@ -1,11 +1,26 @@
-def my_decorator(pizzeria):
+import json
+
+menu = json.load(open('menu.json', 'r', encoding="utf-8"))
+
+
+def decorator_check(pizzeria):
     def wrapper():
-        arr = pizzeria()
-        print("-----Чек-----")
-        print(arr[0])
-        print(arr[1])
-        print(arr[2])
-        print("-----Спасибо за покупку-----")
+        lst = pizzeria()
+        print("\n" + '*' * 32)
+        print("*{: ^30}*".format("*** Чек ***"))
+        print("*{: ^30}*".format("Список товаров:"))
+        for product in lst[3]:
+            print("*{: ^30}*".format(lst[3][product]))
+        print("*{: ^30}*".format("Цена товара:"))
+        print("*{: ^30.2f}*".format(lst[2]))
+        print("*{: ^30}*".format("Вид оплаты:"))
+        print("*{: ^30}*".format({"1": "Наличные", "2": "Карта"}[lst[0]]))
+        print("*{: ^30}*".format("Сумма оплаты:"))
+        print("*{: ^30.2f}*".format(lst[1]))
+        if lst[0] == "1":
+            print("*{: ^30}*".format("Сдача:"))
+            print("*{: ^30.2f}*".format(lst[1] - lst[2]))
+        print('*' * 32)
     return wrapper
 
 
@@ -23,17 +38,14 @@ def choose_product(number, prices_user):
         prices_user[699.99] = "Timosha pizza"
 
     elif number == "4":
-        print("Хорошо!")
-
-    elif number == "5":
         print("Соус добавлен!")
         prices_user[59.99] = "Сырный соус"
 
-    elif number == "6":
+    elif number == "5":
         print("Соус добавлен!")
         prices_user[69.99] = "Кетчуп"
 
-    elif number == "7":
+    elif number == "6":
         print("О наличии этого соуса ходят легенды...")
         prices_user[99999.99] = "Mystery sauce"
 
@@ -75,7 +87,7 @@ def pay(value, prices_user):
     return value_new
 
 
-@my_decorator
+@decorator_check
 def pizzeria():
     value = True
     prices_user = {}
@@ -87,13 +99,14 @@ def pizzeria():
             for i in prices_user:
                 text += str(prices_user[i]) + " "
         print(f"Вас приветствует пиццерия Тимоша джонс!\nВыберите свои любимые блюда!\nВ вашей корзине: {text}")
-        print("Пиццы:\n1) Пепперони! 599,99₽\n2) Сырная пицца! 549,99₽\n3) Timosha pizza! 699,99₽\nСоусы:\n4) Без "
-              "соуса\n5) Сырный! 59,99₽\n6) Кетчуп! 69,99₽\n7) Mystery sauce! 99999,99₽")
+        print("Пиццы:\n1) Пепперони! 599,99₽\n2) Сырная пицца! 549,99₽\n3) Timosha pizza! 699,99₽\nСоусы:\n4) Сырный! "
+              "59,99₽\n5) Кетчуп! 69,99₽\n6) Mystery sauce! 99999,99₽")
 
         choose_product(input("Введите номер блюда!: "), prices_user)
 
         value = pay(input("Продолжить выбор вкуснейших пицц? 1) Да 2) Нет: "), prices_user)
 
-        return [pay, money, sum(prices_user)]
+    return [pay, money, sum(prices_user), prices_user]
+
 
 pizzeria()

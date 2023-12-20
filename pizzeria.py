e9b2e5e76037564fa402b268faab42c94df92b1b
DjMix22 <111224217+DjMix22@users.noreply.github.com>
2023-12-20 11:13:41 +0700

    Update pizzeria.py
    added function of product deletion, menu is imported from json file

--- pizzeria.py
+++ pizzeria.py
@@ -1,11 +1,29 @@
-import json
+from json import load
 
-menu = json.load(open('menu.json', 'r', encoding="utf-8"))
+menu = load(open('menu_pizzeria.json', 'r', encoding='utf-8'))
 
 
-def decorator_check(pizzeria):
+def delete_product(number, user_list):
+    if number != "1" and number != "2":
+        print("Не верное значение")
+        delete_product(input("Желаете ли вы удалить блюдо из вашей корзины?\n1) Да 2) Нет: "), user_list)
+    else:
+        if number == "1":
+            for i in user_list:
+                print(user_list[i])
+            product = input("Введите название товара: ")
+            user_list = {j : i for i, j in user_list.items()}
+            print(f"Товар {product} успешно удален!")
+            del user_list[product]
+            user_list = {j: i for i, j in user_list.items()}
+            return user_list
+        else:
+            return user_list
+
+
+def decorator_check(func):
     def wrapper():
-        lst = pizzeria()
+        lst = func()
         print("\n" + '*' * 32)
         print("*{: ^30}*".format("*** Чек ***"))
         print("*{: ^30}*".format("Список товаров:"))
@@ -24,25 +42,27 @@ def decorator_check(pizzeria):
     return wrapper
 
 
-def choose_product(number, prices_user):
-    product_list = {"1": ["Пепперони! Отличный выбор", 599.99, "Пепперони"], "2": ["Сырная! Отличный выбор", 549.99, "Сырная пицца"],
-                    "3": ["Timosha pizza! Прекрасный выбор!", 699.99, "Timosha pizza"], "4": ["Соус добавлен!", 59.99, "Сырный соус"],
-                    "5": ["Соус добавлен", 69.99, "Кетчуп"], "6": ["О наличии этого соуса ходят легенды...", 99999.99, "Mystery sauce"]}
+def choose_product(number, user_list):
+    product_list = {}
+    for i in menu:
+        for product in menu[i]:
+            product_list[product['number']] = [product['product'], product['price']]
+
     if number in product_list:
-        print(product_list[number][0])
-        prices_user[product_list[number][1]] = product_list[number][2]
+        print(f"{product_list[number][0]}! Отличный выбор!")
+        user_list[product_list[number][1]] = product_list[number][0]
     else:
         print("Такой товар не найден")
-        choose_product(input("Введите номер блюда!: "), prices_user)
+        choose_product(input("Введите номер блюда!: "), user_list)
 
 
-def pay(value, prices_user):
+def pay(value, user_list):
     if value == "1":
         value_new = True
     elif value == "2":
         value_new = False
 
-        print(f"Сумма ваших товаров равна: {sum(prices_user)}")
+        print(f"Сумма ваших товаров равна: {sum(user_list)}")
 
         global pay
         pay = input("Выберите оплату! 1) Наличные 2) Карта: ")
@@ -50,16 +70,16 @@ def pay(value, prices_user):
         global money
         if pay == "1":
             money = int(input("Положите деньги и мы вернем сдачу!: "))
-            if money == sum(prices_user):
+            if money == sum(user_list):
                 print("Оплата закончена!  Удачного дня!")
-            elif money > sum(prices_user):
-                print(f"Ваша сдача: {money - sum(prices_user)}")
+            elif money > sum(user_list):
+                print(f"Ваша сдача: {money - sum(user_list)}")
                 print("Оплата закончена! Удачного дня!")
             else:
                 print("Вам не хватает!")
                 exit()
         elif pay == "2":
-            money = sum(prices_user)
+            money = sum(user_list)
             print("Приложите карту!\nОплата окончена! Удачного дня")
         else:
             print("Такого варианта оплаты нет в нашей сети пиццерий :(")
@@ -73,23 +93,24 @@ def pay(value, prices_user):
 @decorator_check
 def pizzeria():
     value = True
-    prices_user = {}
+    user_list = {}
     while value:
         text = ""
-        if len(prices_user) == 0:
+        if len(user_list) == 0:
             text = "0 товаров"
         else:
-            for i in prices_user:
-                text += str(prices_user[i]) + " "
+            for i in user_list:
+                text += str(user_list[i]) + " "
         print(f"Вас приветствует пиццерия Тимоша джонс!\nВыберите свои любимые блюда!\nВ вашей корзине: {text}")
         print("Пиццы:\n1) Пепперони! 599,99₽\n2) Сырная пицца! 549,99₽\n3) Timosha pizza! 699,99₽\nСоусы:\n4) Сырный! "
               "59,99₽\n5) Кетчуп! 69,99₽\n6) Mystery sauce! 99999,99₽")
 
-        choose_product(input("Введите номер блюда!: "), prices_user)
+        choose_product(input("Введите номер блюда!: "), user_list)
 
-        value = pay(input("Продолжить выбор вкуснейших блюд? 1) Да 2) Нет: "), prices_user)
+        user_list = delete_product(input("Желаете ли вы удалить блюдо из вашей корзины?\n1) Да 2) Нет: "), user_list)
+        value = pay(input("Продолжить выбор вкуснейших блюд?\n1) Да 2) Нет: "), user_list)
 
-    return [pay, money, sum(prices_user), prices_user]
+    return [pay, money, sum(user_list), user_list]
 
 
 pizzeria()
